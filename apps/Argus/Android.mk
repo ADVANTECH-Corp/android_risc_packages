0d74b698646590ef0567f4760a615e7667bd3e0c
Liszt.kao <Liszt.kao@advantech.com.tw>
2018-03-09 15:02:56 +0800

    [AIM-Android]Add Argus Pro apk and so on.
    Signed-off-by: Liszt.kao <Liszt.kao@advantech.com.tw>

---
+++ apps/Argus/Android.mk
@@ -0,0 +1,84 @@
+
+LOCAL_PATH := $(call my-dir)
+
+################################################
+
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := ArgusSysAlarm
+LOCAL_SRC_FILES := ArgusSysAlarm.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_PACKAGE_NAME := ArgusSysAlarm
+LOCAL_CERTIFICATE := shared
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+################################################
+
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := ArgusShellCommand
+LOCAL_SRC_FILES := ArgusShellCommand.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_PACKAGE_NAME := ArgusShellCommand
+LOCAL_CERTIFICATE := shared
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+################################################
+
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := ArgusGetDeviceStatus
+LOCAL_SRC_FILES := ArgusGetDeviceStatus.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_PACKAGE_NAME := ArgusGetDeviceStatus
+LOCAL_CERTIFICATE := shared
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+################################################
+
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := ArgusBlackwhitelistcontroller
+LOCAL_SRC_FILES := ArgusBlackwhitelistcontroller.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_PACKAGE_NAME := ArgusBlackwhitelistcontroller
+LOCAL_CERTIFICATE := shared
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+################################################
+
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := ArgusAgentPro
+LOCAL_SRC_FILES := ArgusAgentPro.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_PACKAGE_NAME := ArgusAgentPro
+LOCAL_CERTIFICATE := shared
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+################################################
