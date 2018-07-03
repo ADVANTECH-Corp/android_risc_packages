ba76551ea64bef3ce52295c8b8719f51348f6b26
Liszt.kao <Liszt.kao@advantech.com.tw>
2018-07-03 17:24:13 +0800

    Revert "Add F button in Development options"
    This reverts commit 87cfcabfef44137218a9b070efa6e42197bffef5.
    
    Signed-off-by: Liszt.kao <Liszt.kao@advantech.com.tw>

--- apps/Settings/src/com/android/settings/DevelopmentSettings.java
+++ apps/Settings/src/com/android/settings/DevelopmentSettings.java
@@ -643,12 +643,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         }
 
         mSwitchBar.addOnSwitchChangeListener(this);
-        int uiDev = SystemProperties.getInt("persist.setting.dev", 0);
-        if (uiDev != Utils.UI_NORMAL) {
-            Switch s = mSwitchBar.getSwitch();
-            mSwitchBar.setEnabled((uiDev & Utils.UI_ENABLE)!=0);
-            s.setEnabled((uiDev & Utils.UI_ENABLE)!=0);
-        }
     }
 
     private boolean removePreferenceForProduction(Preference preference) {
@@ -719,18 +713,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         if (mColorModePreference != null) {
             mColorModePreference.startListening();
             mColorModePreference.updateCurrentAndSupported();
-        }
-        int uiDev = SystemProperties.getInt("persist.setting.dev", 0);
-        if (uiDev != Utils.UI_NORMAL) {
-            boolean enable = (uiDev & Utils.UI_ON)!=0;
-            Switch s = mSwitchBar.getSwitch();
-            mSwitchBar.setChecked(enable);
-            s.setChecked(enable);
-
-            Settings.Global.putInt(getActivity().getContentResolver(),
-                    Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, enable ? 1 : 0);
-            setPrefsEnabledState(enable);
-            mLastEnabledState = enable;
         }
         if (!SystemProperties.getBoolean("persist.setting.dev.stayawake", true)) {
             mDevelopementSettings.removePreference(mKeepScreenOn);
