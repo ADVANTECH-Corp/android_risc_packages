4421103e3dda63ee408af0386c5b72c3fa9eacda
Jimmy.Chung <jimmy.chung@advantech.com.tw>
2019-03-29 10:16:26 +0800

    Fix build error for AIM-Android 2.1.1 patch

--- inputmethods/LatinIME/java/Android.mk
+++ inputmethods/LatinIME/java/Android.mk
@@ -34,7 +34,7 @@ LOCAL_AAPT_FLAGS := -0 .dict
 # Include all the resources regardless of system supported locales
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
-LOCAL_SDK_VERSION := current
+#LOCAL_SDK_VERSION := current
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
